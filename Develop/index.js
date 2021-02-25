dae590bd9e107a74f1785992a2a2bc2430bca86c
jep1220 <38770396+jep1220@users.noreply.github.com>
2021-02-24 22:42:57 -0800

    index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,51 @@
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "GitHub",
+        message: "What is your GitHub username?",
+    },
 
+    {
+        type: "input",
+        name: "email",
+        message: "What is your Email?",
+    },
+
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?",
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Please give a brief description of your project:"
+    },
+
+    {
+        type: "input",
+        name: "install",
+        message: "What does the user need to know about installation?"
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "What does the user need to know about testing your project?"
+    },
+
+        
+    {
+        type: "input",
+        name: "credits",
+        message: "what recources did you use?"
+    },
+
+   
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
